c1f9a55db6dd80457ab28565f75ec1e09b4584fc
Ziad Zebib <zebibziad@gmail.com>
2020-12-31 22:43:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,6 +9,7 @@ end
 def summon_captain_planet(planeteer_calls)
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
+  planeteer_calls.
 end
 
 def long_planeteer_calls(planeteer_calls)
