f168e50c1f238bb253e258c79c695b01af75245c
Ziad Zebib <zebibziad@gmail.com>
2020-12-31 23:02:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,6 +15,7 @@ end
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
+  planeteer_calls.find(>4)  
 end
 
 def find_valid_calls(planeteer_calls)
